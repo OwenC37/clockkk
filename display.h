f70a63bfa8a1e3028e3c7b027f81dc31951565ec
OwenC37 <161964451+OwenC37@users.noreply.github.com>
2024-10-18 20:38:41 -0400

    Update display.h
    if

--- display.h
+++ display.h
@@ -1,20 +1,14 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include "sense.h" // Include this if your functions directly use sense library structures
+#include "sense.h" // Include if its in library structures
 
 int open_display(void);
-
 void display_time(int hours, int minutes, int seconds);
-
-void display_colons(void);
-
-void display_hours(int hours);
-
-void display_minutes(int minutes);
-
-void display_seconds(int seconds);
-
+void colons_displayed(void);
+void hours_displayed(int hours);
+void minutes_displayed(int minutes);
+void seconds_displayed(int seconds);
 void close_display(void);
 
 #endif // DISPLAY_H
