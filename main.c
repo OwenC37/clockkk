d1c867c1b99d59971fa72cacb775e8540514a3d9
OwenC37 <161964451+OwenC37@users.noreply.github.com>
2024-10-18 20:37:48 -0400

    Update main.c
    yes

--- main.c
+++ main.c
@@ -5,21 +5,27 @@
 
 
 #define BLACK 0x0000
+
 int main(void) {
     int hours, minutes, seconds;
 
-    // Initialize the display
+    //  starting the display
+    
     if (!open_display()) {
-        return 1; // Exit with an error code if the display cannot be opened
+        return 1; // Exit if error occurs 
     }
 
 
     while (scanf("%d:%d:%d", &hours, &minutes, &seconds) == 3) {
         pi_framebuffer_t *fb=getFrameBuffer();
+
         clearFrameBuffer(fb,BLACK);
+
         freeFrameBuffer(fb);
-        display_time(hours, minutes, seconds); // Display the time on the LED matrix
-        sleep(1); // Wait for a second before reading the next time input
+
+        display_time(hours, minutes, seconds); // The time will display on the the LED matrix
+        
+        sleep(1); // wait one second wait
     }
 
     close_display();
